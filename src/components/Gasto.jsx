723290eb53ff6cf24ca5d7a4b2a9d74a32f70f64
Joaquin Notebook <joaquin.astengo@gmail.com>
2022-02-13 18:08:15 -0300

    formateando fecha

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { formatearFecha } from '../helpers';
 
 const Gasto = ({gasto}) => {
-  const {categoria, nombre, cantidad, id} = gasto;
+  const {categoria, nombre, cantidad, id, fecha} = gasto;
   return (
     <div className='gasto sombra'>
       <div className='contenido-gasto'>
         <div className='descripcion-gasto'>
+          <p className='nombre-gasto'>{nombre}</p>
           <p className='categoria'>{categoria}</p>
-          <p className='gasto'>{nombre}</p>
-          <p className='gasto'>{cantidad}</p>
+          <p className='fecha-gasto'>
+            Agregado el {''}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
+          <p className='cantidad-gasto'>${cantidad}</p>
       </div>
     </div>
   )
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Gasto from './Gasto';
 
-const ListadoGastos = ({ gastos }) => {
+const ListadoGastos = ({gastos }) => {
+
     return (
         <div className='listado-gastos contenedor'>
             <h2>{gastos.length ? 'Gastos' : 'No hay gastos'}</h2>
