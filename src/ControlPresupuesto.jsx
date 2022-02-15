a6b293f00d4b9bd18f249029ee1924b233ce01d2
Joaquin <joaquin.astengo@gmail.com>
2022-02-15 18:19:33 -0300

    instalando react swipeable

--- src/ControlPresupuesto.jsx
+++ src/ControlPresupuesto.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
-const ControlPresupuesto = ({gastos, presupuesto}) => {
+const ControlPresupuesto = ({ gastos, presupuesto }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
 
-    const [disponible, setDisponible] = useState(0)
-    const [gastado, setGastado] = useState(0)
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total, 0);
+      const totalDisponible = presupuesto - totalGastado
+    setGastado(totalGastado);
+    setDisponible(totalDisponible)
+  }, [gastos]);
 
-    useEffect(() => {
-            const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
-            console.log(totalGastado)
-    }, [gastos])
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("es-ES", {
+      style: "currency",
+      currency: "ARS",
+    });
+  };
 
-    const formatearCantidad = (cantidad) =>{
-        return cantidad.toLocaleString('es-ES', {
-            style: 'currency',
-            currency: 'ARS'
-        })
-    }
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <p>Gráfica aquí</p>
+      </div>
+      <div className="contenido-presupuesto">
+        <p>
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible: </span> {formatearCantidad(disponible)}
+        </p>
+        <p>
+          <span>Gastado: </span> {formatearCantidad(gastado)}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
-            <div>
-                <p>Gráfica aquí</p>
-            </div>
-            <div className='contenido-presupuesto'>
-                <p>
-                    <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
-                </p>
-                <p>
-                    <span>Disponible: </span> {formatearCantidad(disponible)}
-                </p>
-                <p>
-                    <span>Gastado: </span> {formatearCantidad(gastado)}
-                </p>
-            </div>
-        </div>
-    )
-}
-
-export default ControlPresupuesto
+export default ControlPresupuesto;
--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,42 +1,63 @@
-import React from 'react'
-import { formatearFecha } from '../helpers';
-import IconoAhorro from '../img/icono_ahorro.svg'
-import IconoCasa from '../img/icono_casa.svg'
-import IconoComida from '../img/icono_comida.svg'
-import IconoGastos from '../img/icono_gastos.svg'
-import IconoOcio from '../img/icono_ocio.svg'
-import IconoSalud from '../img/icono_salud.svg'
-import IconoSuscripciones from '../img/icono_suscripciones.svg'
-
+import React from "react";
+import { formatearFecha } from "../helpers";
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+import "react-swipeable-list/dist/styles.css";
+import IconoAhorro from "../img/icono_ahorro.svg";
+import IconoCasa from "../img/icono_casa.svg";
+import IconoComida from "../img/icono_comida.svg";
+import IconoGastos from "../img/icono_gastos.svg";
+import IconoOcio from "../img/icono_ocio.svg";
+import IconoSalud from "../img/icono_salud.svg";
+import IconoSuscripciones from "../img/icono_suscripciones.svg";
 
 const diccionarioIconos = {
-  ahorro : IconoAhorro,
-  comida : IconoComida,
-  gastos : IconoGastos,
+  ahorro: IconoAhorro,
+  comida: IconoComida,
+  gastos: IconoGastos,
   casa: IconoCasa,
   ocio: IconoOcio,
   salud: IconoSalud,
   suscripciones: IconoSuscripciones,
-}
+};
 
-const Gasto = ({setGastado, gasto }) => {
+const Gasto = ({ setGastado, gasto }) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
+  const leadingActions = () =>{
+    console.log('Editar...')
+  }
+  const trailingActions = () =>{
+    console.log('Eliminar...')
+  }
+
   return (
-    <div className='gasto sombra'>
-      <div className='contenido-gasto'>
+    <SwipeableList>
+      <SwipeableListItem
+      leadingActions={leadingActions}
+      trailingActions={trailingActions}>
+
+    <div className="gasto sombra">
+      <div className="contenido-gasto">
         <img src={diccionarioIconos[categoria]} alt="" />
-        <div className='descripcion-gasto'>
-          <p className='nombre-gasto'>{nombre}</p>
-          <p className='categoria'>{categoria}</p>
-          <p className='fecha-gasto'>
-            Agregado el {''}
+        <div className="descripcion-gasto">
+          <p className="nombre-gasto">{nombre}</p>
+          <p className="categoria">{categoria}</p>
+          <p className="fecha-gasto">
+            Agregado el {""}
             <span>{formatearFecha(fecha)}</span>
           </p>
         </div>
-        <p className='cantidad-gasto'>${cantidad}</p>
+        <p className="cantidad-gasto">$ {cantidad}</p>
       </div>
     </div>
-  )
-}
+      </SwipeableListItem>
+    </SwipeableList>
+  );
+};
 
 export default Gasto;
