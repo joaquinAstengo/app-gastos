a441285352aa4099ca8fe26101191532ea00f1cf
Joaquin <joaquin.astengo@gmail.com>
2022-02-21 21:48:43 -0300

    señalizando con colores los gastos

--- src/ControlPresupuesto.jsx
+++ src/ControlPresupuesto.jsx
@@ -34,9 +34,9 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
       <div>
         <CircularProgressbar
         styles={buildStyles({
-          pathColor: '#3B82F6',
+          pathColor: porcentaje > 100 ? '#DC2626' : '#3b82f6',
           trailColor: '#F5F5F5',
-          textColor: '#3B82F6'
+          textColor: porcentaje > 100 ? '#DC2626' : '#3b82f6',
         })}
         value={porcentaje}
         text={`${porcentaje}% gastado`}
@@ -46,7 +46,7 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
         <p>
           <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
         </p>
-        <p>
+        <p className={`${disponible < 0 ? 'negativo' : ''}`}>
           <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
         <p>
