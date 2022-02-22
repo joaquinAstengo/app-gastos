4221b33b3a3c458d387c648e4225dffd39be4788
Joaquin <joaquin.astengo@gmail.com>
2022-02-21 21:42:13 -0300

    filtro de gastos agregado

--- src/App.jsx
+++ src/App.jsx
@@ -8,44 +8,50 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
-
-
   const [presupuesto, setPresupuesto] = useState(
     Number(localStorage.getItem('presupuesto')) ?? 0
   )
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+ 
   const [modal, setModal] = useState(false)
   const [animar, setAnimar] = useState(false)
+
   const [gastos, setGastos] = useState(localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : [])
   const [gastoEditar, setGastoEditar] = useState({})
+
   const [filtro, setFiltro] = useState('')
+  const [gastosFiltrados, setGastosFiltrados] = useState([])
+
+
   useEffect(() => {
     if (Object.keys(gastoEditar).length > 0) {
-
       setModal(true)
-
       setTimeout(() => {
         setAnimar(true)
       }, 300);
     }
   }, [gastoEditar])
 
+
   useEffect(() => {
     localStorage.setItem('gastos', JSON.stringify(gastos) ?? 0)
   }, [gastos])
-  
+
+
   useEffect(() => {
     if(filtro) {
-      console.log('Filtrando', filtro)
+      const gastosFiltrados = gastos.filter((gasto)=> gasto.categoria === filtro)
+      setGastosFiltrados(gastosFiltrados)
     }
   }, [filtro])
 
-  const guardarGasto = gasto => {
 
+  const guardarGasto = gasto => {
     if (gasto.id) {
       const gastosActualizados = gastos.map(gastoState => gastoState.id === gasto.id ? gasto : gastoState)
       setGastos(gastosActualizados)
       setGastoEditar({})
+
     } else {
       gasto.id = generarId()
       gasto.fecha = Date.now()
@@ -53,10 +59,12 @@ function App() {
     }
   }
 
+
   const eliminarGasto = id => {
     const gastosActualizados = gastos.filter(gasto => gasto.id !== id);
     setGastos(gastosActualizados)
   }
+
 
   const handleNuevoGasto = () => {
     setModal(true)
@@ -65,6 +73,7 @@ function App() {
       setAnimar(true)
     }, 300);
   }
+
 
   useEffect(() => {
     localStorage.setItem('presupuesto', presupuesto ?? 0)
@@ -93,14 +102,18 @@ function App() {
         {isValidPresupuesto && (
           <>
             <main>
+
               <Filtros
               filtro={filtro}
-              setFiltro={setFiltro}></Filtros>
-              <ListadoGastos
+              setFiltro={setFiltro}>
+              </Filtros>
               
+              <ListadoGastos
                 eliminarGasto={eliminarGasto}
                 gastos={gastos}
                 setGastoEditar={setGastoEditar}
+                filtro={filtro}
+                gastosFiltrados={gastosFiltrados}
               ></ListadoGastos>
             </main>
 
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import Gasto from './Gasto';
 
-const ListadoGastos = ({gastos, setGastoEditar, eliminarGasto }) => {
+const ListadoGastos = ({
+    gastos, setGastoEditar, eliminarGasto,
+    filtro,
+    gastosFiltrados }) => {
 
     return (
+
         <div className='listado-gastos contenedor'>
-            <h2>{gastos.length ? 'Gastos' : 'No hay gastos'}</h2>
-            
-            {gastos.map(gasto =>(
-                <Gasto
-                eliminarGasto={eliminarGasto}
-                key={gasto.id}
-                gasto={gasto}
-                setGastoEditar={setGastoEditar}
-                ></Gasto>
-                ))}
+
+
+            {
+                filtro ? (
+                    <>
+                        <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos'}</h2>
+                        {gastosFiltrados.map(gasto => (
+                            <Gasto
+                                eliminarGasto={eliminarGasto}
+                                key={gasto.id}
+                                gasto={gasto}
+                                setGastoEditar={setGastoEditar}
+                            ></Gasto>
+                        ))}
+
+                    </>
+                ) : (
+                    <>
+                        <h2>{gastos.length ? 'Gastos' : 'No hay gastos'}</h2>
+                        {gastos.map(gasto => (
+                            <Gasto
+                                eliminarGasto={eliminarGasto}
+                                key={gasto.id}
+                                gasto={gasto}
+                                setGastoEditar={setGastoEditar}
+                            ></Gasto>
+                        ))}
+                    </>
+                )
+            }
         </div>
     )
 }
